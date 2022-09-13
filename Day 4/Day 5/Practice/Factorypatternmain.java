231147be5420eb525a92a0378b447255e750b4be
anurag035702 <72249564+anurag035702@users.noreply.github.com>
2022-09-13 10:53:05 +0530

    Delete Day 4/Day 5 directory

--- Day 4/Day 5/Practice/Factorypatternmain.java
+++
@@ -1,7 +0,0 @@
-
-public class Factorypatternmain {
- public static void main(String args[]) {
-	 vehicle v=vehicleFactory.getInstance("car",6);
-	 System.out.println(v.toString());
- }
-}
--- Day 4/Day 5/Practice/vehicle.java
+++
@@ -1,8 +0,0 @@
-
-abstract class vehicle {
-   public abstract int getWheel();
-   
-   public String toString() {
-	   return "wheels for the vehicle created  :"+ this.getWheel();
-   }
-}
